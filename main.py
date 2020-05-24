6f422d2d1a565f68765c3fd61384e0d9050c4341
SerejkaP <haggardspark@gmail.com>
2020-05-25 01:32:28 +0600

    Add comments for functions

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ import timer
 # команды
 bot_commands = ["СТАРТ", "КОНЕЦ", "ПОМОЩЬ", "ЦИКЛ", "ТАЙМЕР:", "ЦИКЛ:", "СОН", "СОН:"]
 
-global boolTimer
-
 # Токен
 file = open('token.TXT', 'r')
 mytoken = file.read()
+
 
 # Функция посылающая сообщение
 def write_msg(user_id, message):
     random_id = vk_api.utils.get_random_id()
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random_id})
 
+
 # Авторизуемся как сообщество
 vk = vk_api.VkApi(token=mytoken)
 longpoll = VkLongPoll(vk)
 
+
 def checker(arg):
-    arrMsg=arg.split()
+    arrMsg = arg.split()
     return arrMsg, len(arrMsg)
+
 
 # Основной цикл
 def new_message():
@@ -36,35 +38,37 @@ def new_message():
                 # Обработка сообщения
                 arrMsg, length = checker(request)
                 # Ответ на команды ботом
-                if arrMsg[0].upper() == bot_commands[0]:
+                if arrMsg[0].upper() == bot_commands[0]:  # Текст для таймера
                     msg = f"Для запуска таймера напишите 'таймер:', что напомнить и время через двоеточие. (Пример:'таймер: слово 3:2:2')"
-                elif arrMsg[0].upper() == bot_commands[1]:
+                elif arrMsg[0].upper() == bot_commands[1]:  # Прощание
                     msg = "До свидания!"
-                elif arrMsg[0].upper() == bot_commands[2]:
+                elif arrMsg[0].upper() == bot_commands[2]:  # Помощь по командам
                     msg = f'Список команд: "СТАРТ", "КОНЕЦ", "ПОМОЩЬ", "ЦИКЛ", "СОН"'
-                elif arrMsg[0].upper() == bot_commands[3]:
+                elif arrMsg[0].upper() == bot_commands[3]:  # Текст для таймера-цикла
                     msg = f"Для запуска таймера  сциклом напишите 'цикл:', что напомнить, время через двоеточие и количество циклов. (Цикл:'таймер: слово 3:2:2 5')"
-                elif arrMsg[0].upper() == bot_commands[4]:
+                elif arrMsg[0].upper() == bot_commands[4]:  # Таймер
                     argTime = arrMsg[2].split(':')
                     msg = timer.default_timer(arrMsg[1], int(argTime[0]), int(argTime[1]), int(argTime[2]))
-                elif arrMsg[0].upper() == bot_commands[5]:
+                elif arrMsg[0].upper() == bot_commands[5]:  # Таймер с циклами
                     argTime = arrMsg[2].split(':')
                     num = int(arrMsg[3])
                     while num > 1:
                         msg = timer.default_timer(arrMsg[1], int(argTime[0]), int(argTime[1]), int(argTime[2]))
                         write_msg(event.user_id, msg)
                         num -= 1
-                elif arrMsg[0].upper() == bot_commands[6]:
+                elif arrMsg[0].upper() == bot_commands[6]:  # Текст для подборки времени сна
                     msg = f"Введите время, когда хотите проснуться. Человеку нужно 14 минут в среднем, чтобы уснуть... (Пример записи: 'Сон: 23:00')"
-                elif arrMsg[0].upper() == bot_commands[7]:
+                elif arrMsg[0].upper() == bot_commands[7]:  # Подбор времени сна
                     argTime = arrMsg[1].split(':')
                     msg = "Время, когда лучше уснуть:" + timer.for_sleeping(int(argTime[0]), int(argTime[1])).split(',')
-                else:
+                else:  # Приветственное сообшение
                     msg = f"Здравствуйте! Если Вам нужен список команд, напишите 'помощь'"
                 write_msg(event.user_id, msg)
 
+
 def main():
     new_message()
+
 
 if __name__ == '__main__':
     main()
--- timer.py
+++ timer.py
@@ -1,25 +1,29 @@
 import time
 
 
+# Таймер
 def default_timer(text, hour, m, sec):
     ans = str(text)
     local_time = hour * 3600 + m * 60 + sec
     time.sleep(local_time)
     return ans
 
+
+# Калькулятор для времени сна
 def for_sleeping(hour, m):
     hour -= 9
     if hour < 0:
         h = hour + 24
     else:
         h = hour
-    local_time = h * 60 + m
-    listTime = [i*90+local_time for i in range(6)]
+    local_time = h * 60 + m  # Перевод времени в минуты
+    listTime = [i * 90 + local_time for i in range(6)]  # Заполнение массива с периодами в полтора часа
     print(listTime)
-    listHours = [listTime[i] // 60 for i in range(6)]
+    listHours = [listTime[i] // 60 for i in range(6)]  # Подсчёт часов
     print(listHours)
-    listMinutes = [listTime[i] - listHours[i]*60 for i in range(6)]
+    listMinutes = [listTime[i] - listHours[i] * 60 for i in range(6)]  # Подсчёт минут
     print(listMinutes)
-    listSleeps = [str(listHours[i]) + ":" + str(listMinutes[i]) for i in range(6)]
+    listSleeps = [str(listHours[i]) + ":" + str(listMinutes[i]) for i in
+                  range(6)]  # Заполнение списка отформатированным временем
     stringSleeps = ' '.join(listSleeps)
     return stringSleeps
